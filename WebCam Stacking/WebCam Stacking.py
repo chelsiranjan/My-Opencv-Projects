e3f926e497bc7b87de1cf1fd2059b879c5022481
chelsiranjan <chelsiranjan04@gmail.com>
2026-06-27 13:39:17 +0530

    this is a project of stacking webcam

---
+++ WebCam Stacking/WebCam Stacking.py
@@ -0,0 +1,62 @@
+import cv2
+import numpy as np
+
+
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
+
+
+
+
+cap = cv2.VideoCapture(0)
+cap.set(10,100)     
+
+while True:
+    sucess, img = cap.read()
+    #cv2.imshow('mywebcam', img)
+
+    kernel = np.ones((5,5),np.uint8)
+
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgCanny = cv2.Canny(img,100,100)
+    imgDialation = cv2.dilate(imgCanny,kernel,iterations = 1)
+    
+    imgStack = stackImages(0.4,([imgGray,imgGray,imgGray,imgGray],[imgGray,imgGray,imgGray,imgGray],[imgGray,imgGray,imgGray,imgGray]))
+    cv2.imshow('my stacked images',imgStack)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+cap.release()            
+cv2.destroyAllWindows()    
+
+
